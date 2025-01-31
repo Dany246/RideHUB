7ca8537978a230e687eb9eb750026adf255ce2e1
Dany246 <145447252+Dany246@users.noreply.github.com>
2025-01-31 10:45:40 +0100

    Login animation

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,6 +1,13 @@
 import { KeySquare, Mail, X } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export const Login = () => {
+  const [loaded, setLoaded] = useState(false);
+  
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     <div 
       className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
@@ -10,17 +17,22 @@ export const Login = () => {
         }
       }}
     >
-      <div className="relative bg-neutral-950 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4">
+      <div 
+        className={`relative bg-neutral-950 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 transition-all duration-1000 transform ${
+          loaded ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+        }`}
+      >
         <a href="/" className="absolute top-4 right-5 text-white">
           <X size={25} strokeWidth={1.5} />
         </a>
         
         <h2 className="text-3xl max-w-md text-center">E-mail: </h2>
-        <Mail className='absolute  left-[3.4rem] top-[6.1rem] text-orange-500'  size={20} strokeWidth={1
-        } /><input className="rounded px-1 text-black" type="email" placeholder="Enter your e-mail" required />
+        <Mail className='absolute left-[3.4rem] top-[6.1rem] text-orange-500' size={20} strokeWidth={1} />
+        <input className="rounded px-1 text-black" type="email" placeholder="Enter your e-mail" required />
         
         <h2 className="text-3xl max-w-md text-center mt-3">Password:</h2>
-        <KeySquare className='text-orange-500  absolute left-[3.5rem] top-[13.1rem]' size={20} strokeWidth={1} /><input className="rounded text-black px-1" type="password" placeholder="Enter your password" required />
+        <KeySquare className='text-orange-500 absolute left-[3.5rem] top-[13.1rem]' size={20} strokeWidth={1} />
+        <input className="rounded text-black px-1" type="password" placeholder="Enter your password" required />
         
         <button className="text-2xl font-extrabold">
           Log<span className="bg-orange-500 text-black px-1 rounded-lg">In</span>

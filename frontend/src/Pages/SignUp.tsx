113ed18facc3dd516fa2250f4f483be9f68d817d
Dany246 <145447252+Dany246@users.noreply.github.com>
2025-02-05 11:25:51 +0100

    nem birom

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -1,15 +1,15 @@
-import {  X } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { X } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export const SignUp = () => {
   const [loaded, setLoaded] = useState(false);
-  
+
   useEffect(() => {
     setLoaded(true);
   }, []);
 
   return (
-    <div 
+    <div
       className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
       onClick={(e) => {
         if (e.target === e.currentTarget) {
@@ -17,7 +17,7 @@ export const SignUp = () => {
         }
       }}
     >
-      <div 
+      <div
         className={`relative bg-neutral-950 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 transition-all duration-1000 transform ${
           loaded ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
         }`}
@@ -27,34 +27,62 @@ export const SignUp = () => {
         </a>
 
         <h2 className="text-3xl max-w-md text-center">First Name: </h2>
-        
-        <input className="rounded px-1 text-black" type="text" placeholder="Enter your First Name" required />
-        
-        <h2 className="text-3xl max-w-md text-center">Last  Name: </h2>
-        
-        <input className="rounded px-1 text-black" type="text" placeholder="Enter your Last Name" required />
+
+        <input
+          className="rounded px-1 text-black"
+          type="text"
+          placeholder="Enter your First Name"
+          required
+        />
+
+        <h2 className="text-3xl max-w-md text-center">Last Name: </h2>
+
+        <input
+          className="rounded px-1 text-black"
+          type="text"
+          placeholder="Enter your Last Name"
+          required
+        />
 
         <h2 className="text-3xl max-w-md text-center">User Name: </h2>
-        
-        <input className="rounded px-1 text-black" type="text" placeholder="Enter your UserName" required />
-        
+
+        <input
+          className="rounded px-1 text-black"
+          type="text"
+          placeholder="Enter your UserName"
+          required
+        />
+
         <h2 className="text-3xl max-w-md text-center">E-mail: </h2>
-      
-        <input className="rounded px-1 text-black" type="email" placeholder="Enter your e-mail" required />
+
+        <input
+          className="rounded px-1 text-black"
+          type="email"
+          placeholder="Enter your e-mail"
+          required
+        />
 
         <h2 className="text-3xl max-w-md text-center">Password: </h2>
-      
-        <input className="rounded px-1 text-black" type="password" placeholder="Enter your password" required />
-        
+
+        <input
+          className="rounded px-1 text-black"
+          type="password"
+          placeholder="Enter your password"
+          required
+        />
+
         <h2 className="text-3xl max-w-md text-center mt-3">Password again:</h2>
 
-        <input className="rounded text-black px-1" type="password" placeholder="Enter your password again" required />
+        <input
+          className="rounded text-black px-1"
+          type="password"
+          placeholder="Enter your password again"
+          required
+        />
 
-
-
-        
         <button className="text-2xl font-extrabold">
-          Sign<span className="bg-orange-500 text-black px-1 rounded-lg">Up</span>
+          Sign
+          <span className="bg-orange-500 text-black px-1 rounded-lg">Up</span>
         </button>
       </div>
     </div>
